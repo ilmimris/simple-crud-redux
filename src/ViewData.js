338a8b2629c3115b5c6aa8278bfd6dcfdd3ef229
ilmimris <mr.is@s.itb.ac.id>
2018-12-09 11:37:10 +0700

    my first try simple CRUD with REDUX

---
+++ src/ViewData.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { connect } from "react-redux";
+import Post from './Post';
+import EditComponent from "./EditComponent";
+
+class ViewData extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className="post_heading">All Posts</h1>
+                {/* {this.props.posts.map((post) => <Post key={post.id} post={post} />)} */}
+                {this.props.posts.map((post) => (
+                    <div key={post.id}>
+                        {post.editing ? <EditComponent post={post} key={post.id} /> :
+                            <Post key={post.id} post={post} />}
+                    </div>
+                ))}
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        posts: state
+    }
+}
+
+export default connect(mapStateToProps)(ViewData);
